f46c384f73a507f1f353c5df50a1dce299578540
Rutuja1923 <rutujajirage19@gmail.com>
2025-02-05 11:24:32 +0530

    Add URL model with visit history

---
+++ models/Url.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const urlSchema  = new Schema(
+    {
+        shortID : {
+            type: String,
+            requied: true,
+            unique: true,
+        },
+        redirectURL: {
+            type: String,
+            required: true,
+        },
+        visitHistory: [
+            {
+                timeStamp: {
+                    type: Date,
+                    default: () => new Date(),
+                }
+            }
+        ],
+    },
+    {
+        timestamps: true,
+    },
+);
+
+const URL = mongoose.model('url', urlSchema);
+
+module.exports = URL; 
