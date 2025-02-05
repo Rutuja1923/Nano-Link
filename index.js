d89441d54c8a1744f05719ee97d86940c03cf213
Rutuja1923 <rutujajirage19@gmail.com>
2025-02-05 12:17:56 +0530

    a simple view of all short urls

--- index.js
+++ index.js
@@ -17,6 +17,29 @@ app.use(express.json());
 //initialize router
 app.use('/url',urlRoute);
 
+//simple rendering 
+app.get('/test', async (req,res) => {
+    const URL = require('./models/Url');
+    const allURLs = await URL.find({});
+    return res.end(
+        `
+        <html>
+            <head>
+                <title>Short URLs</title>
+            </head>
+            <body>
+                <h1>All Short URLs</h1>
+                <ol>
+                    ${allURLs.map(shortUrl => 
+                        `<li>${shortUrl.shortID} - ${shortUrl.redirectURL} - ${shortUrl.visitHistory.length}</li>`
+                    ).join('')}
+                </ol>
+            </body>
+        </html>
+        `
+    );
+});
+
 //start the server
 app.listen(PORT, () => {
     console.log(`Server started at port ${PORT}`);
