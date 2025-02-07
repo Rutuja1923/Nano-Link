e3744e05060ea5a79578ee68c3a7726a599c18d2
Rutuja1923 <rutujajirage19@gmail.com>
2025-02-07 19:53:04 +0530

    replicate route changes

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const path = require('path');
+const cookieParser = require('cookie-parser');
 
 const {connectMongoDB} = require('./connection');
 
+//require routes
 const urlRoute = require('./routes/url');
 const staticRoute = require('./routes/staticRouter');
+const userRoute = require('./routes/user');
+
+//require middleware
+const {restrictToLoggedinUserOnly} = require('./middlewares/auth');
 
 const PORT = 3000;
-const app = express();
+const app = express(); // express application object
 
 //DB connection
 const url = 'mongodb://127.0.0.1:27017/short-url';
@@ -23,10 +29,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 //middleware
 app.use(express.json());
 app.use(express.urlencoded( {extended: false} ));
+app.use(cookieParser());
 
 //initialize router
-app.use('/url',urlRoute);
+app.use('/url', restrictToLoggedinUserOnly, urlRoute);
 app.use('/',staticRoute);
+app.use('/user',userRoute);
 
 //start the server
 app.listen(PORT, () => {
