1236b9a6058fa475c5f51e003c841923dee6c9ed
Rutuja1923 <rutujajirage19@gmail.com>
2025-02-07 23:11:50 +0530

    sign the token against secret key

--- service/auth.js
+++ service/auth.js
@@ -1,11 +1,27 @@
-const sessionIDtoUserMap = new Map();
+//stateless authentication
+const jwt = require('jsonwebtoken');
 
-function setUser(id, user) {
-    sessionIDtoUserMap.set(id,user);
+require("dotenv").config();
+const SECRET = process.env.SECRET_KEY;
+
+function setUser(user) {
+    return jwt.sign({
+        _id: user._id,
+        email: user.email,
+    }, SECRET);
 }
 
-function getUser(id) {
-    return sessionIDtoUserMap.get(id);
+function getUser(token) {
+    if (!token) {
+        return null;
+    }
+    try {
+        return jwt.verify(token, SECRET);
+    } 
+    catch (error) {
+        console.error("JWT verification failed:", error);
+        return null;
+    }
 }
 
 module.exports = {setUser, getUser};
