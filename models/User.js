a8cecd757fda28980a91d423c5dec28cf94d0528
Rutuja1923 <rutujajirage19@gmail.com>
2025-03-15 22:11:04 +0530

    added password hashing & role based users

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const Schema = mongoose.Schema;
 
@@ -17,11 +18,28 @@ const userSchema  = new Schema(
             type: String,
             required: true,
         },
+        role: {
+            type: String,
+            required: true,
+            default: "NORMAL",
+        }
     },
     {
         timestamps: true,
     },
 );
+
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
+        return next();
+    }
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+});
+
+userSchema.methods.comparePassword = async function (candidatePassword) {
+    return bcrypt.compare(candidatePassword, this.password);
+};
 
 const User = mongoose.model('user', userSchema);
 
