e7987abf04ac1dfc7d9838c5751be8aaa3332720
Rutuja1923 <rutujajirage19@gmail.com>
2025-03-15 11:50:34 +0530

    sending bearer token in auth header

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,14 +1,19 @@
 const { getUser } = require("../service/auth");
 
 async function restrictToLoggedinUserOnly(req, res, next) {
-    const userUid = req.cookies?.uid;
+    // const userUid = req.cookies?.uid;
 
-    if (!userUid) {
+    //Checking Headers for Bearer Token
+    const userUid = req.headers['authorization'];
+
+    if (!userUid || !userUid.startsWith("Bearer ")) {
         return res.redirect('/login');
     }
 
+    const token = userUid.split("Bearer ")[1];
+
     try {
-        const user = getUser(userUid);
+        const user = getUser(token);
 
         if (!user) {
             return res.redirect('/login');
@@ -21,13 +26,22 @@ async function restrictToLoggedinUserOnly(req, res, next) {
         console.error("Error during token verification:", error);
         return res.redirect('/login');
     }
-}
+} 
 
 async function checkAuth(req, res, next) {
-    const userUid = req.cookies?.uid;
+    //const userUid = req.cookies?.uid;
+
+    //Checking Headers for Bearer Token
+    const userUid = req.headers['authorization'];
+
+    if (!userUid || !userUid.startsWith("Bearer ")) {
+        return res.status(401).json({ message: 'Authorization header missing or invalid' });
+    }
+
+    const token = userUid.split("Bearer ")[1];
 
     try {
-        const user = getUser(userUid);
+        const user = getUser(token);
         req.user = user;
         next();
     } 
