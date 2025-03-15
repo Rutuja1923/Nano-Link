a91dfd92a3ae1a38a2c11acc8c30184ac7e76698
Rutuja1923 <rutujajirage19@gmail.com>
2025-03-15 22:08:45 +0530

    Added jwt expiry time & finding user based on verified payload

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,80 +1,32 @@
 const { getUser } = require("../service/auth");
 
-function checkForAuthentication (req, res, next) {
-    const authorizationHeaderValue = req.headers["authorization"];
+async function checkForAuthentication (req, res, next) {
+    const tokenCookie = req.cookies?.token;
     req.user = null ;
 
-    if (!authorizationHeaderValue || !authorizationHeaderValue.startsWith("Bearer ")) {
-        return next();
+    if (tokenCookie) {
+        const user = await getUser(tokenCookie);
+        if (user) {
+            req.user = user;
+        }
     }
 
-    const token = authorizationHeaderValue.split("Bearer ")[1];
-    const user = getUser(token);
-
-    req.user = user;
+    console.log("User in checkForAuthentication:", req.user);
+    
     return next();
 }
 
-// async function restrictToLoggedinUserOnly(req, res, next) {
-//     // const userUid = req.cookies?.uid;
-
-//     //Checking Headers for Bearer Token
-//     const userUid = req.headers['authorization'];
-
-//     if (!userUid || !userUid.startsWith("Bearer ")) {
-//         return res.redirect('/login');
-//     }
-
-//     const token = userUid.split("Bearer ")[1];
-
-//     try {
-//         const user = getUser(token);
-
-//         if (!user) {
-//             return res.redirect('/login');
-//         }
-
-//         req.user = user;
-//         next();
-//     } 
-//     catch (error) {
-//         console.error("Error during token verification:", error);
-//         return res.redirect('/login');
-//     }
-// } 
-
-// async function checkAuth(req, res, next) {
-//     //const userUid = req.cookies?.uid;
-
-//     //Checking Headers for Bearer Token
-//     const userUid = req.headers['authorization'];
-
-//     if (!userUid || !userUid.startsWith("Bearer ")) {
-//         return res.status(401).json({ message: 'Authorization header missing or invalid' });
-//     }
-
-//     const token = userUid.split("Bearer ")[1];
-
-//     try {
-//         const user = getUser(token);
-//         req.user = user;
-//         next();
-//     } 
-//     catch (error) {
-//         console.error("Error during token verification:", error);
-//         return res.status(401).json({ message: 'Invalid or expired token' });
-//     }
-// }
-
 function restrictTo(roles) {
     return function(req, res, next) {
+        console.log("User in restrictTo:", req.user);
         if (!req.user) {
             return res.redirect("/login");
         }
 
-        if (!roles.inclused(req.user.role)) {
+        if (!roles.includes(req.user.role)) {
             return res.end("Unauthorized");
         }
+        
         return next();
     }
 }
