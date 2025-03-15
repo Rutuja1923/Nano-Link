463ff6400aea4979330f26be4e4935665777d0c4
Rutuja1923 <rutujajirage19@gmail.com>
2025-03-15 22:12:09 +0530

    role based restriction, admin route, logout route

--- routes/staticRouter.js
+++ routes/staticRouter.js
@@ -1,18 +1,26 @@
 const express = require('express');
 const URL = require('../models/Url');
 const router = express.Router();
+const {restrictTo} = require("../middlewares/auth");
 
-router.get('/', async (req, res) => {
-    if (!req.user) {
-        return res.redirect('/login');
-    }
+router.get('/', restrictTo(["NORMAL", "ADMIN"]), async (req, res) => {
     const allURLs = await URL.find({
         createdBy: req.user._id
     });
+
     return res.render('home', { 
         id: req.query.id || null,
         url: req.query.url || '', 
         urls: allURLs,
+    });
+});
+
+router.get('/admin/urls', restrictTo(["ADMIN"]), async (req, res) => {
+    const allURLs = await URL.find({});
+    
+    return res.render('home', {  
+        urls: allURLs,
+        id: req.user ? req.user._id : null
     });
 });
 
@@ -22,6 +30,12 @@ router.get('/signup', (req,res) => {
 
 router.get('/login', (req,res) => {
     return res.render('login');
+});
+
+router.get("/logout", (req, res) => {
+    res.clearCookie("token")
+
+    return res.redirect("/login");
 });
 
 module.exports = router;
