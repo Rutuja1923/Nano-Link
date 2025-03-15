46ebf802070057a17b43f9a4ff33bbf9c765c3de
Rutuja1923 <rutujajirage19@gmail.com>
2025-03-15 12:42:04 +0530

    Code refactor for authentication & authorization

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,54 +1,82 @@
 const { getUser } = require("../service/auth");
 
-async function restrictToLoggedinUserOnly(req, res, next) {
-    // const userUid = req.cookies?.uid;
+function checkForAuthentication (req, res, next) {
+    const authorizationHeaderValue = req.headers["authorization"];
+    req.user = null ;
 
-    //Checking Headers for Bearer Token
-    const userUid = req.headers['authorization'];
-
-    if (!userUid || !userUid.startsWith("Bearer ")) {
-        return res.redirect('/login');
+    if (!authorizationHeaderValue || !authorizationHeaderValue.startsWith("Bearer ")) {
+        return next();
     }
 
-    const token = userUid.split("Bearer ")[1];
+    const token = authorizationHeaderValue.split("Bearer ")[1];
+    const user = getUser(token);
 
-    try {
-        const user = getUser(token);
+    req.user = user;
+    return next();
+}
 
-        if (!user) {
-            return res.redirect('/login');
+// async function restrictToLoggedinUserOnly(req, res, next) {
+//     // const userUid = req.cookies?.uid;
+
+//     //Checking Headers for Bearer Token
+//     const userUid = req.headers['authorization'];
+
+//     if (!userUid || !userUid.startsWith("Bearer ")) {
+//         return res.redirect('/login');
+//     }
+
+//     const token = userUid.split("Bearer ")[1];
+
+//     try {
+//         const user = getUser(token);
+
+//         if (!user) {
+//             return res.redirect('/login');
+//         }
+
+//         req.user = user;
+//         next();
+//     } 
+//     catch (error) {
+//         console.error("Error during token verification:", error);
+//         return res.redirect('/login');
+//     }
+// } 
+
+// async function checkAuth(req, res, next) {
+//     //const userUid = req.cookies?.uid;
+
+//     //Checking Headers for Bearer Token
+//     const userUid = req.headers['authorization'];
+
+//     if (!userUid || !userUid.startsWith("Bearer ")) {
+//         return res.status(401).json({ message: 'Authorization header missing or invalid' });
+//     }
+
+//     const token = userUid.split("Bearer ")[1];
+
+//     try {
+//         const user = getUser(token);
+//         req.user = user;
+//         next();
+//     } 
+//     catch (error) {
+//         console.error("Error during token verification:", error);
+//         return res.status(401).json({ message: 'Invalid or expired token' });
+//     }
+// }
+
+function restrictTo(roles) {
+    return function(req, res, next) {
+        if (!req.user) {
+            return res.redirect("/login");
         }
 
-        req.user = user;
-        next();
-    } 
-    catch (error) {
-        console.error("Error during token verification:", error);
-        return res.redirect('/login');
-    }
-} 
-
-async function checkAuth(req, res, next) {
-    //const userUid = req.cookies?.uid;
-
-    //Checking Headers for Bearer Token
-    const userUid = req.headers['authorization'];
-
-    if (!userUid || !userUid.startsWith("Bearer ")) {
-        return res.status(401).json({ message: 'Authorization header missing or invalid' });
-    }
-
-    const token = userUid.split("Bearer ")[1];
-
-    try {
-        const user = getUser(token);
-        req.user = user;
-        next();
-    } 
-    catch (error) {
-        console.error("Error during token verification:", error);
-        return res.status(401).json({ message: 'Invalid or expired token' });
+        if (!roles.inclused(req.user.role)) {
+            return res.end("Unauthorized");
+        }
+        return next();
     }
 }
 
-module.exports = { restrictToLoggedinUserOnly, checkAuth };
+module.exports = {checkForAuthentication, restrictTo};
